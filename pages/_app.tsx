1927867f8aea47aa42c549945f4afb281761f479
Daniel Lichtenstein <daniel@trell.se>
2020-09-14 15:22:55 +0200

    Include a third party lib

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,23 +2,26 @@ import App from "next/app";
 import { Provider as StyletronProvider } from "styletron-react";
 import { styletron, debug } from "../styletron";
 import { StilrenProvider } from "stilren";
+import { StylesProvider } from "@material-ui/core/styles";
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <StyletronProvider value={styletron} debug={debug} debugAfterHydration>
-        <StilrenProvider
-          styletron={styletron}
-          mediaPrefixes={{
-            desktop: "(min-width: 1280px)",
-            tablet: "(min-width: 769px) and (max-width: 1279px)",
-            mobile: "(max-width: 768px)",
-          }}
-        >
-          <Component {...pageProps} />
-        </StilrenProvider>
-      </StyletronProvider>
+      <StylesProvider injectFirst>
+        <StyletronProvider value={styletron} debug={debug} debugAfterHydration>
+          <StilrenProvider
+            styletron={styletron}
+            mediaPrefixes={{
+              desktop: "(min-width: 1280px)",
+              tablet: "(min-width: 769px) and (max-width: 1279px)",
+              mobile: "(max-width: 768px)",
+            }}
+          >
+            <Component {...pageProps} />
+          </StilrenProvider>
+        </StyletronProvider>
+      </StylesProvider>
     );
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,26 @@
+import { Button as MUIButton } from "@material-ui/core";
 import Button from "../components/Button";
+
+const rainbowAnimation = {
+  "0%": {
+    color: "red",
+  },
+  "20%": {
+    color: "yellow",
+  },
+  "40%": {
+    color: "green",
+  },
+  "60%": {
+    color: "blue",
+  },
+  "80%": {
+    color: "purple",
+  },
+  "100%": {
+    color: "red",
+  },
+};
 
 const IndexPage = () => (
   <>
@@ -11,8 +33,30 @@ const IndexPage = () => (
     </p>
     <p>
       The core premise of stilren is adding "all" css properties as first level
-      props in <b>every</b> component without any extra overhead.
+      props in{" "}
+      <b
+        $textTransform="uppercase"
+        $animationDuration="5s"
+        $animationIterationCount="infinite"
+        $animationName={rainbowAnimation}
+      >
+        every
+      </b>{" "}
+      component without any extra overhead. Just use the <i>$</i> character as a
+      prefix before any standard css property.
     </p>
+    <p>
+      One of the cool things about stilren is that it works with zero effort for
+      a ton of third party library components as well.
+    </p>
+    <div $display="flex">
+      <MUIButton $color="red" $flex="1">
+        Material UI button
+      </MUIButton>
+      <MUIButton $color="blue" $flex="2">
+        Material UI button
+      </MUIButton>
+    </div>
   </>
 );
 
